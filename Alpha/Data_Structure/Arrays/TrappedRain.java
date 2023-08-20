e96a9d07f3230a29e2c8c120fa69dd5e18f55f02
Yadnesh Ahirrao <yadneshahirrao.19@gmail.com>
2023-08-20 13:04:37 +0530

    FU

--- Alpha/Data_Structure/Arrays/TrappedRain.java
+++ Alpha/Data_Structure/Arrays/TrappedRain.java
@@ -1,12 +1,10 @@
 package Alpha.Data_Structure.Arrays;
 
-import java.util.Arrays;
-
 public class TrappedRain {
     public static void main(String[] args) {
         int arr[] = { 4, 2, 0, 6, 3, 2, 5 };
-        //int arr[] = { 1,8,6,2,5,4,8,3,7 };
-        //trappedwater(arr);
+        // int arr[] = { 1,8,6,2,5,4,8,3,7 };
+        // trappedwater(arr);
         System.out.println(trappedrainwater(arr));
     }
 
@@ -31,33 +29,33 @@ public class TrappedRain {
             rm[ii] = rmax;
 
             water = Math.min(lm[i], rm[i]);
-            totalwater = totalwater + (Math.abs(arr[i]-water));
+            totalwater = totalwater + (Math.abs(arr[i] - water));
         }
 
         System.out.println(totalwater);
     }
 
-    public static int trappedrainwater(int arr[]){
+    public static int trappedrainwater(int arr[]) {
         int n = arr.length;
-        //left most bondry array
+        // left most bondry array
         int leftmax[] = new int[n];
         leftmax[0] = arr[0];
         for (int i = 1; i < n; i++) {
-            leftmax[i] = Math.max(arr[i],leftmax[i-1]); 
+            leftmax[i] = Math.max(arr[i], leftmax[i - 1]);
         }
 
-        //right max
+        // right max
         int rightmax[] = new int[n];
-        rightmax[n-1] = arr[n-1];
-        for (int i = n-2; i >= 0; i--) {
-            rightmax[i] = Math.max(arr[i], rightmax[i+1]);
+        rightmax[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightmax[i] = Math.max(arr[i], rightmax[i + 1]);
         }
 
         int trappedwater = 0;
         for (int i = 0; i < n; i++) {
             int waterlevel = Math.min(leftmax[i], rightmax[i]);
             trappedwater += waterlevel - arr[i];
-     
+
         }
 
         return trappedwater;
--- Alpha/Data_Structure/Striver_A_to_Z_DSA/Striver_Arrays_1.java
+++ Alpha/Data_Structure/Striver_A_to_Z_DSA/Striver_Arrays_1.java
@@ -2,16 +2,15 @@ package Alpha.Data_Structure.Striver_A_to_Z_DSA;
 
 public class Striver_Arrays_1 {
     public static void main(String[] args) {
-        int arr[] = {3, 2, 1, 5, 4, 6};
+        int arr[] = { 3, 2, 1, 5, 4, 6 };
         System.out.println(LargestElement(arr));
     }
 
-
     // 1. Find the largest element in the array
-    public static int LargestElement(int[] arr){
+    public static int LargestElement(int[] arr) {
         int larger = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] > larger){
+            if (arr[i] > larger) {
                 larger = arr[i];
             }
         }
