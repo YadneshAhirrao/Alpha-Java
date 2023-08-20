9ae57d7c2775f5da65e6a2ce5e8c2213235f71a3
Yadnesh Ahirrao <yadneshahirrao.19@gmail.com>
2023-08-20 22:14:17 +0530

    PU

--- Alpha/Data_Structure/Sorting/Sorting.java
+++ Alpha/Data_Structure/Sorting/Sorting.java
@@ -7,7 +7,9 @@ public class Sorting {
         int[] arr = {5, 4, 3, 0, 1, 2};
         System.out.println(Arrays.toString(arr));
         //bubblesort(arr);
-        selectionsort(arr);
+        //selectionsort(arr);
+        // Insertionsort(arr);
+        Insertionsort_simple(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -37,6 +39,51 @@ public class Sorting {
             int temp = arr[minpose];
             arr[minpose] = arr[i];
             arr[i] = temp;
+        }
+    }
+
+    public static void Insertionsort(int[] arr){
+        for (int i = 1; i <= arr.length; i++) {
+            int curr = arr[i];
+            int prev = i-1;
+
+            while (prev >=0 && arr[prev] > curr) {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+
+            arr[prev+1] = curr;
+        }
+    }
+
+    public static void Insertionsort_simple(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int j = i;
+
+            while (j>0 && arr[j-1] > arr[j]) {
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+                j--;
+            }
+        }
+    }
+
+    public static void CountingSort(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(arr[i],max);
+        }
+
+        int count[] = new int[max+1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++; 
+        }
+        int j = 0;
+        for (int i = 0; i < max; i++) {
+            arr[j] = i;
+            j++;
+            count[i]--;
         }
     }
 }
