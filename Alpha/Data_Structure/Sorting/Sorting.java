89d60d6ba809ea39ec5cd28f22e7712519750755
Yadnesh Ahirrao <yadneshahirrao.19@gmail.com>
2023-08-20 17:54:35 +0530

    Fu

--- Alpha/Data_Structure/Sorting/Sorting.java
+++ Alpha/Data_Structure/Sorting/Sorting.java
@@ -2,7 +2,7 @@ package Alpha.Data_Structure.Sorting;
 
 import java.util.Arrays;
 
-public class Bubble {
+public class Sorting {
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 0, 1, 2};
         System.out.println(Arrays.toString(arr));
--- Alpha/Data_Structure/Striver_A_to_Z_DSA/Striver_Arrays_1.java
+++ Alpha/Data_Structure/Striver_A_to_Z_DSA/Striver_Arrays_1.java
@@ -4,9 +4,15 @@ import java.util.Arrays;
 
 public class Striver_Arrays_1 {
     public static void main(String[] args) {
-        int arr[] = { 3, 2, 1, 5, 4, 6 };
-        System.out.println(LargestElement(arr));
-        System.out.println(SecondLargest(arr));
+        // int arr[] = { 3, 2, 1, 5, 4, 6 };
+        int arr[] = { 1, 1, 2, 2, 2, 3, 3, 3 };
+        System.out.println(Arrays.toString(arr));
+        // Arrays.sort(arr);
+        // System.out.println(CheckSorted(arr));
+        // System.out.println(LargestElement(arr));
+        // System.out.println(SecondLargest_1(arr));
+        // System.out.println(SecondLargest_2(arr));
+        System.out.println(RemoveDublicate(arr));
     }
 
     // 1.1 Find the largest element in the array
@@ -21,9 +27,9 @@ public class Striver_Arrays_1 {
     }
 
     // 1.2 Find the second largest element in an array
-    public static int SecondLargest(int[] arr) {
+    public static int SecondLargest_1(int[] arr) {
         for (int i = 0; i < 2; i++) {
-            for (int j = 1; j < arr.length; j++) {
+            for (int j = 1; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
@@ -35,4 +41,39 @@ public class Striver_Arrays_1 {
         return arr[arr.length - 2];
     }
 
+    public static int SecondLargest_2(int[] arr) {
+        int larger = arr[0];
+        int second = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > larger) {
+                second = larger;
+                larger = arr[i];
+            }
+        }
+
+        return second;
+    }
+
+    // 1.3 check if array is sorted
+    public static int CheckSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] <= arr[i - 1])
+                return 0;
+        }
+        return 1;
+    }
+
+    // 1.4 Remove Dublicate from Sorted Array
+
+    public static int RemoveDublicate(int[] arr) {
+        int a = 0;
+        for (int i = 1; i < arr.length ; i++) {
+            if (arr[a] != arr[i]) {
+                arr[a + 1] = arr[i];
+                a++;
+             }
+        }
+        return a + 1;
+    }
 }
