2908b5afbf01d949a7c8db3f8f81bd1967f686b3
Yadnesh Ahirrao <yadneshahirrao.19@gmail.com>
2023-08-21 22:45:18 +0530

    FU

---
+++ Alpha/Data_Structure/Arrays/TD_Arrays/SpiralMatrix.java
@@ -0,0 +1,17 @@
+package Alpha.Data_Structure.Arrays.TD_Arrays;
+
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        int arr[][] = {
+                        {1, 2, 3 ,4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12}
+        };
+        PrintSpiral(arr);
+    }
+
+
+    public static void PrintSpiral(int arr[][]){
+        int
+    }
+}
